97094ff8d0f5d7a647adf33bb096101b662d4030
wangkai15 <wangkai15@100tal.com>
2019-08-29 21:26:23 +0800

    re

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import './Loading.css';
+import style from './Loading.cm.styl';
+
+
+export default () => {
+    return (
+        <div className={style.autoSuggest}>
+            loading...
+        </div>
+    )
+}
